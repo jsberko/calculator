362c51f9440ffe487c39fe7b06abf9d6dcf158a6
jsberko <147778552+jsberko@users.noreply.github.com>
2025-08-14 06:41:00 -0400

    Create num1, num2 and operator variables

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
+// Variables
+let num1 = 3;
+let num2 = 5;
+let operator = "+";
+
 // Math Operation Functions
 function add(a, b) { return a + b };
 function subtract(a, b) { return a - b };
 function multiply(a, b) { return a * b };
 function divide(a, b) { return a / b };
+
+// function operate(num1, num2, operator) {
+//     if 
+// }
 
 
 // Event Listeners
